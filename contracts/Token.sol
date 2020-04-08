adec02abb5463cfc83418c20749d882d6f37efda
nitinmittal23 <nitinmittal44066@gmail.com>
2020-04-09 03:22:48 +0530

    full stack dapp

---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.16;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract TutorialToken is ERC20 {
+  // add your token code here
+  string public name = "MyToken";
+  string public symbol = 'MT';
+  uint256 public INITIAL_SUPPLY = 1200000;
+  
+  constructor() public {
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
+}
